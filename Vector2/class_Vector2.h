64e1ed2b72f69200a7552eb4139cf879064401eb
Vikotor_Torgunakov_715_FRTK <39098751+ViktorTorgunakov715FRTK@users.noreply.github.com>
2019-02-23 11:23:48 +0300

    Update class_Vector2.h

--- Vector2/class_Vector2.h
+++ Vector2/class_Vector2.h
@@ -1,36 +1,49 @@
 // class_Vector2.h - VT_0.0
 #pragma once
-#include <math.h>
+#include <cmath>
 #include <iostream>
+#include <assert.h>
+
+
+const double PI = 3.141593;
 
 
 class Vector2
 {
     private:
-        double x;
-        double y;
-        double z;
+        double x_;
+        double y_;
+
     public:
+        ~Vector2( ) { }
         Vector2( )
         {
-            x = y = z = 0.;
+            x_ = y_ = 0.;
         }
-        Vector2(double x_arg, double y_arg, double z_arg)
+        Vector2(double x_arg, double y_arg)
         {
-            x = x_arg;
-            y = y_arg;
-            z = z_arg;
+            x_ = x_arg;
+            y_ = y_arg;
         }
 
-        int operator=(const Vector2 & vector);
-        Vector2 operator+(const Vector2 & vector);
-        Vector2 operator-(const Vector2 & vector);
+        Vector2 & operator=(const Vector2 & vector);
+        Vector2 operator+(const Vector2 & vector) const;
+        Vector2 & operator+=(const Vector2 & vector);
+        Vector2 operator-(const Vector2 & vector) const;
+        Vector2 operator-(void) const;
+        Vector2 & operator-=(const Vector2 & vector);
         Vector2 operator*(double k) const;
-        double operator*(const Vector2 & vector);
-        Vector2 operator^(const Vector2 & vector);
+        Vector2 operator/(double k) const;
+        double operator*(const Vector2 & vector) const;
+        double operator^(const Vector2 & vector) const;
 
-        double len(void);
-        void reset(double x_arg, double y_arg, double z_arg);
+        double len(void) const;
+        double square_len(void) const;
+        void reset(double x_arg, double y_arg);
+        Vector2 orthogonal(void) const;
+        void rotate(double degree);
+        Vector2 get_rotated(double degree) const;
+        Vector2 norm(void);
 
         friend Vector2 operator*(double k, const Vector2 & vector);
         friend std::ostream & operator<<(std::ostream & os, const Vector2 & vector);
