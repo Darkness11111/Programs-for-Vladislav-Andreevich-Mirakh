dc8c582a51c704f5728ff886d91d9b2ec84ef7d8
Vikotor_Torgunakov_715_FRTK <39098751+ViktorTorgunakov715FRTK@users.noreply.github.com>
2019-02-23 11:24:39 +0300

    Update Vector2_func.cpp

--- Vector2/Vector2_func.cpp
+++ Vector2/Vector2_func.cpp
@@ -1,99 +1,190 @@
-// Vector2_func.cpp - VT_0.0
-#pragma once
+// Vector2_func.cpp - VT_1.0
 #include "class_Vector2.h"
 
 
-int Vector2::operator=(const Vector2 & vector)
+Vector2 & Vector2::operator=(const Vector2 & vector) 
 {
-    x = vector.x;
-	y = vector.y;
-	z = vector.z;
+    x_ = vector.x_;
+	y_ = vector.y_;
     
 
-	if (x || y || z)
-	{
-		return 1;
-	}
-    return 0;
+	return *this;
 }
 
 
-Vector2 Vector2::operator+(const Vector2 & vector)
+Vector2 Vector2::operator+(const Vector2 & vector) const
 {
     Vector2 sum;
-	sum.x = x + vector.x;
-	sum.y = y + vector.y;
-	sum.z = z + vector.z;	
+	sum.x_ = x_ + vector.x_;
+	sum.y_ = y_ + vector.y_;	
 
 
 	return sum;
 }
 
 
-Vector2 Vector2::operator-(const Vector2 & vector)
+Vector2 & Vector2::operator+=(const Vector2 & vector)
+{
+	x_ += vector.x_;
+	y_ += vector.y_;
+
+
+	return *this;
+}
+
+
+Vector2 Vector2::operator-(const Vector2 & vector) const
 {
     Vector2 difference;
-	difference.x = x - vector.x;
-	difference.y = y - vector.y;
-	difference.z = z - vector.z;	
+	difference.x_ = x_ - vector.x_;
+	difference.y_ = y_ - vector.y_;
 
 
 	return difference;
 }
 
 
+Vector2 Vector2::operator-(void) const
+{
+    return Vector2(-x_, -y_);
+}
+
+
+Vector2 & Vector2::operator-=(const Vector2 & vector)
+{
+	x_ -= vector.x_;
+	y_ -= vector.y_;	
+
+
+	return *this;
+}
+
+
 Vector2 Vector2::operator*(double k) const
 {
     Vector2 composition;
-	composition.x = x * k;
-	composition.y = y * k;
-	composition.z = z * k;
+	composition.x_ = x_ * k;
+	composition.y_ = y_ * k;
 
 
 	return composition;
 }
 
 
-double Vector2::operator*(const Vector2 & vector)
+Vector2 Vector2::operator/(double k) const
+{
+	assert(k != 0);
+
+
+    Vector2 division;
+	division.x_ = x_ / k;
+	division.y_ = y_ / k;
+
+
+	return division;
+}
+
+
+double Vector2::operator*(const Vector2 & vector) const
 {
     double scalar;
-	scalar = x * vector.x + y * vector.y + z * vector.z;
+	scalar = x_ * vector.x_ + y_ * vector.y_;
 
 
     return scalar;
 }
 
 
-Vector2 Vector2::operator^(const Vector2 & vector)
+double Vector2::operator^(const Vector2 & vector) const
 {
-	Vector2 vect_composition;
-	vect_composition.x = y * vector.z - z * vector.y;
-	vect_composition.y = z * vector.x - x * vector.z;
-	vect_composition.z = x * vector.y - y * vector.x;
+	double vect_comp_len;
+	vect_comp_len = x_ * vector.y_ - y_ * vector.x_;
 
 
-    return vect_composition;
+    return vect_comp_len;
 }
 
 
-double Vector2::len(void)
+double Vector2::len(void) const
 {
     double len;
-	len = sqrt(x * x + y * y + z * z);
+	len = sqrt(x_ * x_ + y_ * y_);
 
 
 	return len;
 }
 
 
-void Vector2::reset(double x_arg, double y_arg, double z_arg)
+double Vector2::square_len(void) const
 {
-    x = x_arg;
-	y = y_arg;
-	z = z_arg;
+    double sqr_len;
+	sqr_len = x_ * x_ + y_ * y_;
+
+
+	return sqr_len;
+}
+
+
+void Vector2::reset(double x_arg, double y_arg)
+{
+    x_ = x_arg;
+	y_ = y_arg;
 
 
 	return;
+}
+
+
+Vector2 Vector2::orthogonal(void) const
+{
+    Vector2 orthgnl = Vector2(y_, -x_);
+    return orthgnl;
+}
+
+
+void Vector2::rotate(double degree)
+{
+	double angle;
+	if (!x_ && !y_)
+	{
+        return;
+	}
+
+    angle = atan2(y_, x_);
+    angle += (degree * PI) / 180;
+
+    double len = this -> len( );
+    x_ = len * cos(angle);
+    y_ = len * sin(angle);
+    return;
+}
+
+
+Vector2 Vector2::get_rotated(double degree) const
+{
+	double angle;
+	if (!x_ && !y_)
+	{
+        return *this;
+	}
+
+    angle = atan2(y_, x_);
+    angle += (degree * PI) / 180;
+
+    double len = this -> len( );
+    return  Vector2(len * cos(angle), len * sin(angle));
+}
+
+
+Vector2 Vector2::norm(void)
+{
+	if (!x_ && !y_)
+	{
+		return Vector2(0., 0.);
+	}
+
+    double len = this -> len( );
+    return Vector2(x_ / len, y_ / len);
 }
 
 
@@ -105,7 +196,7 @@ Vector2 operator*(double k, const Vector2 & vector)
 
 std::ostream & operator<<(std::ostream & os, const Vector2 & vector)
 {
-    os << "(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+    os << "(" << vector.x_ << ", " << vector.y_ << ")";
 
 
 	return os; 
